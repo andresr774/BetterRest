9dc8b4f3973808161f97fc5bcd5406bd9333a259
Andres <andresr774@gmail.com>
2022-02-13 11:33:34 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andres camilo Raigoza misas on 13/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
